dcfb63cdfc891c37d369536eeab9d23002cf2b81
gabriel <gabriel.pdg8@gmail.com>
2023-04-13 18:28:58 -0300

    final

---
+++ src/repository/carRepository.ts
@@ -0,0 +1,36 @@
+import prisma from "../config/database.js";
+
+async function getCars() {
+  const data = await prisma.cars.findMany();
+  return data;
+}
+
+async function getCar(id: number) {
+  const data = await prisma.cars.findUnique({where: {id}});
+  return data;
+}
+
+async function getCarWithLicensePlate(licensePlate: string) {
+  const data = await prisma.cars.findUnique({where: {licensePlate}});
+  return data;
+}
+
+async function createCar(model: string, licensePlate: string, year: number, color: string) {
+  const data = await prisma.cars.create({data: {model, licensePlate, year, color}});
+  return data;
+}
+
+async function deleteCar(id: number) {
+  const data =  await prisma.cars.delete({where: {id}});
+  return data;
+}
+
+const carRepository = {
+  getCar,
+  getCarWithLicensePlate,
+  getCars,
+  createCar,
+  deleteCar
+}
+
+export default carRepository;
